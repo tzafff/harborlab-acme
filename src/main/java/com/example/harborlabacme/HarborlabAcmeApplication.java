8df58eb9adcc00a31e37d9f5d6d0c2c4d36f73d0
tzafff <christzaf99@gmail.com>
2024-12-14 16:40:49 +0200

    Spring Boot application init.

---
+++ src/main/java/com/example/harborlabacme/HarborlabAcmeApplication.java
@@ -0,0 +1,13 @@
+package com.example.harborlabacme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarborlabAcmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarborlabAcmeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/harborlabacme/HarborlabAcmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.harborlabacme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarborlabAcmeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
